de18cb371618a85760087c975a11deff71aa1ca9
Stuart Hanscombe <hanscs1969@yahoo.co.uk>
2016-10-01 12:45:09 +0200

    Adding convenience methods for base module

--- lib/k2poker.ex
+++ lib/k2poker.ex
@@ -1,2 +1,25 @@
 defmodule K2poker do
+
+  # These are just helper methods, the only required
+  # methods for the API in order to initialize and play
+  # a game, they call the GamePlay methods of the same name
+
+  alias K2poker.GamePlay, as: GamePlay
+
+  def new(player1, player2) do
+    GamePlay.new(player1, player2)
+  end
+
+  def play(game, player_id) do
+    GamePlay.play(game, player_id)
+  end
+
+  def discard(game, player_id, card_index) do
+    GamePlay.discard(game, player_id, card_index)
+  end
+
+  def fold(game, player_id) do
+    GamePlay.fold(game, player_id)
+  end
+
 end
--- test/k2poker_test.exs
+++ test/k2poker_test.exs
@@ -2,4 +2,35 @@ defmodule K2pokerTest do
   use ExUnit.Case
   doctest K2poker
 
+  # The base module has helper methods to the only required methods
+  # in K2poker.GamePlay, so as a convenience we can start and play
+  # a game with game = K2poker.new("bob", "stu") etc
+
+  test "K2poker.new/2 starts a new game" do
+    game = K2poker.new("bob", "stu")
+    assert game.status == :deal
+  end
+
+  test "K2poker.play/2 continues play" do
+    game = K2poker.new("bob", "stu")
+    game = K2poker.play(game, "stu")
+    assert List.last(game.players).status == :ready
+  end
+
+  test "K2poker.discard/3 discards the given card" do
+    game = K2poker.new("bob", "stu")
+    old_card = List.first(List.last(game.players).cards)
+    game = K2poker.discard(game, "stu", 0)
+    new_card = List.first(List.last(game.players).cards)
+    refute old_card == new_card
+    assert Enum.count(game.deck) == 46
+  end
+
+  test "K2poker.fold/2 finished the game with player folded" do
+    game = K2poker.new("bob", "stu")
+    game = K2poker.fold(game, "stu")
+    assert List.last(game.players).status == :folded
+    assert game.status == :finished
+  end
+
 end
